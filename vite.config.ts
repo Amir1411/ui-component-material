d9699e07111e080afffc07689ec7fd3c36e227d0
Mohd Amir <md.amir14@gmail.com>
2024-08-21 19:10:01 +0200

    chore: Update package.json name and version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+// import svg from "vite-plugin-svg";
 
-// https://vitejs.dev/config/
+import { dependencies, peerDependencies } from "./package.json";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+    dts({
+      include: ["src/**/*"],
+    }),
+    // svg(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src", "index.ts"),
+      formats: ["es", "cjs"],
+      fileName: (ext) => `index.${ext}.js`,
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(peerDependencies),
+        ...Object.keys(dependencies),
+      ],
+      output: { preserveModules: true, exports: "named" },
+    },
+
+    target: "esnext",
+    sourcemap: true,
+  },
+  base: "/<REPO>/",
+});
